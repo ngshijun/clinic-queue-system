d8e5ba86729bfe4132c1b03c733c7b537e363e02
ngshijun <shijunng2010@gmail.com>
2025-06-10 20:00:39 +0800

    test

--- api/test.js
+++ api/test.js
@@ -1,3 +1,15 @@
-export function GET(request) {
-  return new Response(process.env.API_TOKEN);
+export async function GET(request) {
+  const response = await fetch('http://protege.powerapi.powersoft.asia/api/protege/get_last_queue_no', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      registerno: '4215',
+      visitdate: new Date().toISOString().split('T')[0],
+    }),
+  })
+  const data = await response.json()
+  return new Response(data.data[0]?.queuno || 0)
 }
