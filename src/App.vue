233b484fc4d51afa72fc93283be3d98de6207fc8
ngshijun <shijunng2010@gmail.com>
2025-06-10 17:31:59 +0800

    fix: api url error

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ const lastUpdated = ref<Date | null>(null)
 const token = ref<string | null>(null)
 let intervalId: number | null = null
 
+const BASE_URL = import.meta.env.PROD ? 'http://protege.powerapi.powersoft.asia' : '/api'
+
 const estimateTime = () => {
   if (patientNumber.value !== null && currentNumber.value < patientNumber.value) {
     const diff = patientNumber.value - currentNumber.value
@@ -37,7 +39,7 @@ const fetchCurrentNumber = async () => {
 
   isLoading.value = true
   try {
-    const response = await fetch('/api/api/protege/get_last_queue_no', {
+    const response = await fetch(`${BASE_URL}/api/protege/get_last_queue_no`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -71,7 +73,7 @@ const fetchToken = async () => {
     formData.append('username', import.meta.env.VITE_API_USERNAME)
     formData.append('password', import.meta.env.VITE_API_PASSWORD)
 
-    const response = await fetch('/api/auth/', {
+    const response = await fetch(`${BASE_URL}/auth/`, {
       method: 'POST',
       body: formData,
     })
